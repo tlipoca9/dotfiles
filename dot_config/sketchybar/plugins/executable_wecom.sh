80d24664f17f84b988da4ebaeb3e788860f23657
tlipoca9 <tlipoca@gmail.com>
2025-02-24 19:38:16 +0800

    chore: polish dotfiles

--- dot_config/sketchybar/plugins/executable_wecom.sh
+++ dot_config/sketchybar/plugins/executable_wecom.sh
@@ -6,11 +6,11 @@ WECOM_PROCESS=$(ps aux | grep "WXWork" | grep -v "grep")
 if [[ $WECOM_PROCESS != "" ]]; then
     UNREAD=$(lsappinfo -all info -only StatusLabel "企业微信" | sed -nr 's/\"StatusLabel\"=\{ \"label\"=\"(.+)\" \}$/\1/p')
     if [[ $UNREAD == "" ]]; then
-        sketchybar --set wecom label.drawing=off icon.color=0xff2590fe
+        sketchybar --set $NAME label.drawing=off icon.color=0xff2590fe
     else
         # 企业微信未读消息, 红色标签
-        sketchybar --set wecom label="● $UNREAD" label.drawing=on label.color=0xffd0021b icon.color=0xffd0021b
+        sketchybar --set $NAME label="● $UNREAD" label.drawing=on label.color=0xffd0021b icon.color=0xffd0021b
     fi
 else
-    sketchybar --set wecom label.drawing=off icon.color=0xff2590fe
+    sketchybar --set $NAME label.drawing=off icon.color=0xff2590fe
 fi
